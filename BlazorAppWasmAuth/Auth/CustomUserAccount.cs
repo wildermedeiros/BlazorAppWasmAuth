5a4aecae7c20933df9b87d97359eae2e6f651415
Wilder Medeiros <wilder.sistemas@lar.ind.br>
2024-05-03 17:02:15 -0300

    refactor: organizing auth's folder structure. docs: adding docs to CustomUserAccount.cs

--- BlazorAppWasmAuth/Auth/CustomUserAccount.cs
+++ BlazorAppWasmAuth/Auth/CustomUserAccount.cs
@@ -1,7 +1,15 @@
 ﻿namespace BlazorAppWasmAuth.Auth;
 
+/// <summary>
+/// Represents a custom user account that extends RemoteUserAccount with additional properties.
+/// </summary>
+/// <remarks>
+/// This class adds custom properties to the standard RemoteUserAccount to handle specific claim types
+/// that are part of the authentication process. It is particularly useful for cases where additional
+/// claims such as 'example' are crucial for validating the contexts in which the tokens are valid.
+/// </remarks>
 public class CustomUserAccount : RemoteUserAccount
 {
-    [JsonPropertyName("aud")]
-    public string? Aud { get; set; }
+    [JsonPropertyName("example")]
+    public string? Example { get; set; }
 }
